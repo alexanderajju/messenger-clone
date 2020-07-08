e97a8a9d9194f7583c29a453c26bbf4ae71aa648
alexanderajju <alexanderajju@gmail.com>
2020-07-08 09:20:28 +0530

    added form control

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from '@material-ui/core';
+import { Button,InputLabel,Input,FormControl } from '@material-ui/core';
 import './App.css';
 
 function App() {
@@ -20,8 +20,11 @@ function App() {
      <h1>Hii There</h1>
 
     <form>
-    <input value={input} onChange={event => setInput(event.target.value)} />
-     <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage} >Send Message</Button>
+    <FormControl>
+  <InputLabel>Enter a message...</InputLabel>
+  <Input value={input} onChange={event => setInput(event.target.value)}/>
+  <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage} >Send Message</Button>
+    </FormControl>
     </form>
     
 
