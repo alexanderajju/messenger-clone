5d4d27355f687c0942b339b964f91e00df982ae9
alexanderajju <alexanderajju@gmail.com>
2020-07-08 10:01:29 +0530

    useEffect and added username

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button,InputLabel,Input,FormControl } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setmessages] = useState([]);
-  console.log(input)
-  console.log(messages)
+  const [messages, setmessages] = useState([
+    {username: 'Aju',text: 'hey there'}
+  ]);
+  const [username, setUsername] = useState('');
 
+  useEffect(() => {
+    setUsername(prompt('Please enter your name'));
+  },[])
   const sendMessage = (event) =>{
     event.preventDefault();
     setmessages([...messages, input])
@@ -19,6 +23,7 @@ function App() {
     <div className="App">
    
      <h1>Hii There</h1>
+  <h2>Welcome {username}</h2>
 
     <form>
     <FormControl>
@@ -31,7 +36,7 @@ function App() {
 
     {
       messages.map(message => (
-        <Message text={message}/>
+        <Message username={message.username} text={message.text}/>
       ))
     }
 
